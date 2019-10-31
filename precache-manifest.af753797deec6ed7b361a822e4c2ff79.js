cf8ee757da619689ee833dee5bc9e8ecf35f00c8
Will Strong <38888375+willsstrong@users.noreply.github.com>
2019-10-30 21:24:28 -0600

    Updates

--- precache-manifest.af753797deec6ed7b361a822e4c2ff79.js
+++ precache-manifest.af753797deec6ed7b361a822e4c2ff79.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "40cbb01a8030d918ac419b842b18c7bb",
+    "revision": "4add854976d638ae7acde11f3751bcea",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "b2991a2c02a6ed91e278",
-    "url": "/portfolio/static/css/main.b100e6da.chunk.css"
+    "revision": "8fc007d0cdcfb0abc054",
+    "url": "/portfolio/static/css/main.34de6062.chunk.css"
   },
   {
     "revision": "3661eeb1bc5baee2d45b",
     "url": "/portfolio/static/js/2.a471c8e0.chunk.js"
   },
   {
-    "revision": "b2991a2c02a6ed91e278",
-    "url": "/portfolio/static/js/main.b2f9a636.chunk.js"
+    "revision": "8fc007d0cdcfb0abc054",
+    "url": "/portfolio/static/js/main.96658980.chunk.js"
   },
   {
     "revision": "814a19a647d373cc1e96",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.5db36143da07bbbe7b8bffccce0c0881.js"
+  "/portfolio/precache-manifest.af753797deec6ed7b361a822e4c2ff79.js"
 );
 
 self.addEventListener('message', (event) => {
