a73b1b48cf3c75f673205bdd7de67fa9f8f09c89
Will Strong <38888375+willsstrong@users.noreply.github.com>
2019-11-08 11:37:14 -0700

    Added Jumbotron to top of page

--- src/App.js
+++ src/App.js
@@ -4,19 +4,16 @@
  * Main App Layout for Portfolio WePage
  */
 
- import 'bootstrap/dist/css/bootstrap.min.css';
- import 'holderjs';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'holderjs';
 
 import React, { Component } from 'react';
-import { 
-  Button, 
-  Image, 
-  Row, 
-  Col, 
-  Container 
+import {
+  Jumbotron,
+  //JumbotronProps,
+  //Container 
 } from 'react-bootstrap';
 
-import Product from './Product';
 import Header from './Header';
 import Projects from './Projects';
 
@@ -25,28 +22,14 @@ class App extends Component {
   render() {
     return (
       <>
-        <Header />
         <div>
-          <h1>My First React App!</h1>
-          <Product />
-          <Button variant="primary">Primary</Button>
+          <Jumbotron fluid>
+            <h1>My First React App!</h1>
+          </Jumbotron>
+          <Header />
         </div>
+
         <Projects />
-        <div>
-          <Container>
-            <Row>
-              <Col xs={6} md={4}>
-                <Image src="holder.js/171x180" rounded />
-              </Col>
-              <Col xs={6} md={4}>
-                <Image src="holder.js/171x180" roundedCircle />
-              </Col>
-              <Col xs={6} md={4}>
-                <Image src="holder.js/171x180" thumbnail />
-              </Col>
-            </Row>
-          </Container>
-        </div>
       </>
     );
   }
