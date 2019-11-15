98d87ce035f44da201a3e5cf8d81d79e706c29e2
Will Strong <38888375+willsstrong@users.noreply.github.com>
2019-11-15 00:49:19 -0700

    fixed spelling mistakes and formating in Title

--- src/components/Title.js
+++ src/components/Title.js
@@ -79,8 +79,8 @@ export const Title = () => (
                 <h1>Hello, World!</h1>
                 <div id="cursor"></div>
                 <h3>My name is Will Strong, and I love to code!</h3>
-                <p> Weather it's writing a desktop application for maintaining a customer database;
-                    creating visually stunning web apps;
+                <p> Whether it's writing a desktop application for managing a customer database;<br/>
+                    creating visually stunning web apps;<br/>
                     or going to bare metal with Assembly or C,
                     I find great fullfilment in the science and art of software engineering.
                 </p>
